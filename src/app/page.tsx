a22af88fc38d2655bfa0e9247b9b38812c5e18f8
Beod Wilson <beodwilson@gmail.com>
2025-07-30 11:28:28 +0000

    Change the Date Taken and Date Closed columns to be simple date strings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,12 +54,8 @@ const formatDateForCsv = (date: Date | null): string => {
     const day = String(date.getUTCDate()).padStart(2, '0');
     const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Month is 0-indexed
     const year = date.getUTCFullYear();
-    
-    const hours = String(date.getUTCHours()).padStart(2, '0');
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
 
-    return `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
+    return `${month}/${day}/${year}`;
 };
 
 const simulateTrade = (
